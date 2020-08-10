083330928ec556dcb91a769e951a4f77e0c8ff70
Onduso <londuso@us.ci.org>
2020-08-11 00:30:22 +0300

    Implemented picking the values from TextEditingControllers and rest button

--- lib/screens/goal_setting_form.dart
+++ lib/screens/goal_setting_form.dart
@@ -9,81 +9,96 @@ class GoalSettingForm extends StatefulWidget {
 class _GoalSettingFormState extends State<GoalSettingForm> {
   final _formKey = GlobalKey<FormState>();
 
-  TextEditingController _goalTxtBox= TextEditingController();
+  final _goalTxtBox = TextEditingController();
 
-  TextEditingController _taskOne= TextEditingController();
+  final _taskOne = TextEditingController();
 
-  TextEditingController _taskTwo= TextEditingController();
+  final _taskTwo = TextEditingController();
 
-  TextEditingController _taskThree= TextEditingController();
+  final _taskThree = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Goal Setting')),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Form(
-              key: _formKey,
-              child: Column(children: <Widget>[
+        body: SingleChildScrollView(
+          //helps avoid widget overflow
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Form(
+                key: _formKey,
+                child: Column(children: <Widget>[
+                  _drawFormTextBox('Enter Goal', _goalTxtBox), //Goal Field
+                  SizedBox(height: 8.0),
 
-                _drawFormTextBox('Enter Goal', _goalTxtBox),//Goal Field
-                 SizedBox(height:8.0),
+                  _drawFormTextBox('Enter Task 1', _taskOne), //Task 1
+                  SizedBox(height: 8.0),
 
-                _drawFormTextBox('Enter Task 1', _taskOne),//Task 1
-                SizedBox(height:8.0),
+                  _drawFormTextBox('Enter Task 2', _taskTwo), //Task 2
+                  SizedBox(height: 8.0),
 
-                _drawFormTextBox('Enter Task 2', _taskTwo),//Task 2
-                SizedBox(height:8.0),
+                  _drawFormTextBox('Enter Task 3', _taskThree), //Task 3
 
-                _drawFormTextBox('Enter Task 3', _taskThree),//Task 3
-
-                Row(children: <Widget>[ 
-                  _drawButton('Submit',_submit),
-                  SizedBox(width:8.0),
-                  _drawButton('Reset',_reset),
-                  
-                  ],)
-               
-
-                
-
-                   
-                
-              ])),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _drawButton('Submit', _submit),
+                      SizedBox(width: 8.0),
+                      _drawButton('Reset', _reset),
+                    ],
+                  )
+                ])),
+          ),
         ));
   }
+
 //Draw Text Boxes for Goals and Tasks
-  Widget _drawFormTextBox(String txtBoxLabel, TextEditingController txtEditingController) {
-    
-    
+  Widget _drawFormTextBox(
+      String txtBoxLabel, TextEditingController txtEditingController) {
     return TextFormField(
       validator: (value) => value.isEmpty ? 'Kindly $txtBoxLabel' : null,
       controller: txtEditingController,
-     // style: TextStyle(color: Colors.yellow, fontSize: 20.0),
+      //onChanged: (text) =>print(text),
+      // style: TextStyle(color: Colors.yellow, fontSize: 20.0),
       decoration: InputDecoration(
         labelText: txtBoxLabel,
         labelStyle: TextStyle(color: Colors.white, fontSize: 20.0),
         //hintText: 'Enter Goal',
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
-        
       ),
     );
   }
 
-  Widget _drawButton(String btnText, Function() fun ){
+  Widget _drawButton(String btnText, Function() submitOrRestForm) {
     return FlatButton(
-                             onPressed: ()=>fun(), 
-                             child: Text(btnText, style:TextStyle(color:Colors.white,fontSize:18)),
-                             color: Colors.indigo, 
-                             );
+      onPressed: () => submitOrRestForm(),
+      child: Text(btnText, style: TextStyle(color: Colors.white, fontSize: 18)),
+      color: Colors.indigo,
+    );
   }
 
-  void _submit(){
-    print('Submitted');
+  void _submit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print(_goalTxtBox.text);
+      print(_taskOne.text);
+      print(_taskTwo.text);
+      print(_taskThree.text);
+
+      //Call the API to pick values to Goal Table( ID=GoalID, GoalName=Goal1, Student_FK_id,GoalStatus, goalperiodId)
+      //Call this to task to Task Table(GoalFKID, taskID, Taskname)
+      //Navigate to the 'add another goal
+
+    }
   }
 
-  void _reset(){
-    print('Reset');
+  void _reset() {
+    setState(() {
+      _goalTxtBox.text = '';
+      _goalTxtBox.text = '';
+      _taskOne.text = '';
+      _taskTwo.text = '';
+      _taskThree.text = '';
+    });
   }
 }
